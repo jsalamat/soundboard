88496980117f9a7779391289a46df41b9d1dbc19
Jon Salamat <Jon Salamat>
2017-09-27 01:14:15 -0400

    test selection of audio element

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,6 +2,9 @@ window.addEventListener('keydown', function(e) {
 // to test the event keycode when pressing keyboards down
 	// console.log(e.keyCode);
 // Look for data-element of key and select its audio element 
-//by using an attribute selecot
-	const audio = document.querrySelector("audio")
+// use attribute selector like example line 6
+	// const audio = document.querySelector("audio[data-key=65]")
+	const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+// to test selection of audio element
+		console.log(audio);
 });
