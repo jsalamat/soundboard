f6ce97f77f05d85d8617715ef8181e80b872242f
Jon Salamat <Jon Salamat>
2017-09-26 23:18:56 -0400

    select audio and css style for miku style theme

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,7 @@
 window.addEventListener('keydown', function(e) {
 // to test the event keycode when pressing keyboards down
-	console.log(e.keyCode);
+	// console.log(e.keyCode);
+// Look for data-element of key and select its audio element 
+//by using an attribute selecot
+	const audio = document.querrySelector("audio")
 });
