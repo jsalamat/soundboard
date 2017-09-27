eac6dacd727af8f1637278a6f3452cf9ac530f90
Jon Salamat <Jon Salamat>
2017-09-27 01:39:01 -0400

    testing key and getting ready for animation

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,17 +1,22 @@
 window.addEventListener('keydown', function(e) {
-// to test the event keycode when pressing keyboards down
-	// console.log(e.keyCode);
+	// to test the event keycode when pressing keyboards down
+		// console.log(e.keyCode);
 
 // Look for data-element of key and select its audio element 
-// use attribute selector like example line 6
+	// use attribute selector like example line 6
 	// const audio = document.querySelector("audio[data-key=65]")
-
 	const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+	// to test selection of audio element
+			// console.log(audio);
 
-// to test selection of audio element
-		// console.log(audio);
+//to select key element
+	const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
+	// to test selection of key element
+		console.log(key);
+
 //stop the function from running all together if there no audio
 	if(!audio) return;
+
 //problem it plays once and needs to play in succession when keys press
 //solve this by adding current time it rewind it to the start
 	audio.currentTime = 0;
