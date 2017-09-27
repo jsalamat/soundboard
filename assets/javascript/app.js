c642bd032d49e6c37aa38f50bb4367b5cb2d5d14
Jon Salamat <Jon Salamat>
2017-09-27 01:31:06 -0400

    audio working and audio element fix

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,20 @@
 window.addEventListener('keydown', function(e) {
 // to test the event keycode when pressing keyboards down
 	// console.log(e.keyCode);
+
 // Look for data-element of key and select its audio element 
 // use attribute selector like example line 6
 	// const audio = document.querySelector("audio[data-key=65]")
+
 	const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+
 // to test selection of audio element
-		console.log(audio);
+		// console.log(audio);
+//stop the function from running all together if there no audio
+	if(!audio) return;
+//problem it plays once and needs to play in succession when keys press
+//solve this by adding current time it rewind it to the start
+	audio.currentTime = 0;
+	audio.play();
+
 });
